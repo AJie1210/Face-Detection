c0884a97369e3f30b7ae55df5f5d35de87aae087
AJie1210 <curt665566@gmail.com>
2022-06-20 07:40:17 +0800

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -94,7 +94,10 @@ cv2.imshow('pic2erode', erodes)
 cv2.waitKey(0)
 """
 
-img = np.full((600, 600), 255, np.uint8)
+"""
+make line, rectangle, circle, text
+
+img = np.full((600, 600), 255, np.uint8) # create a new image(white:255) 
 img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) # change color mod(from gray to rgb)
 
 cv2.line(img, (0, 0), (600, 600), (255, 0, 0), 5)
@@ -104,3 +107,48 @@ cv2.putText(img, 'Wayne', (100,100), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 3
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
+"""
+
+"""
+def empty(v):
+    pass
+
+img = cv2.imread('pic\\pic2.jpg')
+
+cv2.namedWindow('TrackBar')
+cv2.resizeWindow('TrackBar', 640, 480)
+
+cv2.createTrackbar('Hue min', 'TrackBar', 0, 179, empty)
+cv2.createTrackbar('Hue max', 'TrackBar', 179, 179, empty)
+cv2.createTrackbar('Sat min', 'TrackBar', 0, 255, empty)
+cv2.createTrackbar('Sat max', 'TrackBar', 255, 255, empty)
+cv2.createTrackbar('Val min', 'TrackBar', 0, 255, empty)
+cv2.createTrackbar('Val max', 'TrackBar', 255, 255, empty)
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+while True:
+    h_min = cv2.getTrackbarPos('Hue min', 'TrackBar') #get trackbar position
+    h_max = cv2.getTrackbarPos('Hue max', 'TrackBar')
+    s_min = cv2.getTrackbarPos('Sat min', 'TrackBar')
+    s_max = cv2.getTrackbarPos('Sat max', 'TrackBar')
+    v_min = cv2.getTrackbarPos('Val min', 'TrackBar')
+    v_max = cv2.getTrackbarPos('Val max', 'TrackBar')
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max]) 
+
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(img, img, mask = mask)
+
+    cv2.imshow('img', img)
+    cv2.imshow('hsv', hsv)
+    cv2.imshow('mask', mask)
+    cv2.imshow('result', result)
+    
+    cv2.waitKey(1)
+"""
+
+
+
